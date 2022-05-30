d2ae4c9de166dd69c23a3a35c1f281fff5e8efab
Matt Deitke <28768645+mattdeitke@users.noreply.github.com>
2022-05-30 02:21:57 -0700

    make entries -1

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ def load_dataset() -> prior.DatasetDict:
     data = {}
     for split, size in [("train", 1000), ("val", 100), ("test", 100)]:
         data[split] = prior.Dataset(
-            [100 for _ in tqdm(range(size))], dataset="test", split="test"
+            [-1 for _ in tqdm(range(size))], dataset="test", split="test"
         )
     return prior.DatasetDict(**data)
