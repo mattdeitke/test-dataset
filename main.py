ea5910db0d8c261ed25fc4b86aed8aed7a62f24f
Matt Deitke <28768645+mattdeitke@users.noreply.github.com>
2022-05-30 02:02:08 -0700

    use constant

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ def load_dataset() -> prior.DatasetDict:
     data = {}
     for split, size in [("train", 1000), ("val", 100), ("test", 100)]:
         data[split] = prior.Dataset(
-            [i for i in tqdm(range(size))], dataset="test", split="test"
+            [100 for _ in tqdm(range(size))], dataset="test", split="test"
         )
     return prior.DatasetDict(**data)
