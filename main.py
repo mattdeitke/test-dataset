21993c22a02fa69d5fa593c10f57f3e9865cc803
Matt Deitke <28768645+mattdeitke@users.noreply.github.com>
2022-05-30 02:00:02 -0700

    reverse dataset

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ def load_dataset() -> prior.DatasetDict:
     data = {}
     for split, size in [("train", 1000), ("val", 100), ("test", 100)]:
         data[split] = prior.Dataset(
-            [i for i in tqdm(range(size))], dataset="test", split="test"
+            [size - i - 1 for i in tqdm(range(size))], dataset="test", split="test"
         )
     return prior.DatasetDict(**data)
